4f3859053b143b1e3434a929e5370f8550ad9ffc
sourabhpande532 <sourabhpande43@gmail.com>
2026-07-27 15:50:31 +0530

    fix: update environment configuration and resolve CORS issues

--- index.js
+++ index.js
@@ -9,9 +9,21 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 databaseInitialization();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+  "https://talenthub-client-rho.vercel.app",
+];
+
 const corsOption = {
-  origin: process.env.FRONTEND_URL,
-  credential: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
@@ -23,9 +35,9 @@ app.use(express.json());
 
 app.use("/auth", require("./routes/auth.routes"));
 
-app.get("/",(req,res)=>{
-  res.send('Welcome to TalentHub express server')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to TalentHub express server");
+});
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
